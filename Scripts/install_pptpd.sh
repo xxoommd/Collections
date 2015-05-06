30f080b83f76c2d12b4acd894916a853cd81f465
gaofei <gaofei@gaofeideMacBook-Air.local>
2015-05-06 10:28:44 +0800

    Add comment for pptpd script

--- Scripts/install_pptpd.sh
+++ Scripts/install_pptpd.sh
@@ -13,7 +13,8 @@ else
 	echo "option /etc/ppp/pptpd-options\nlogwtmp\nlocalip 10.20.0.1\nremoteip 10.20.0.2-238,10.20.0.10" > /etc/pptpd.conf
 
 	# Config /etc/ppp/chap-secrets
-	echo "gaofei pptpd really001 *" > /etc/ppp/chap-secrets
+	echo "# 用户名 类型 密码 连接IP" > /etc/ppp/chap-secrets
+	echo "gaofei pptpd really001 *" >> /etc/ppp/chap-secrets
 
 	# Config /etc/ppp/pptpd-options
 	echo "name pptpd\nrefuse-pap\nrefuse-chap\nrefuse-mschap\nrequire-mschap-v2\nrequire-mppe-128\nms-dns 8.8.8.8\nms-dns 8.8.4.4\nproxyarp\nnodefaultroute\nlock\nnobsdcomp\nnovj\nnovjccomp\nnologfd" > /etc/ppp/pptpd-options
